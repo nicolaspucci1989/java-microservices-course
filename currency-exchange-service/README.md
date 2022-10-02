39aefc29786ebfb433c7f3ca362ca6b222b1425b
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-10-02 20:41:47 -0300

    add currency exchange service
    currency exchange service

---
+++ currency-exchange-service/README.md
@@ -0,0 +1,5 @@
+### Currency Exchange Service
+`http://localhost8000/currency-exchange/from/USD/to/INR`
+
+### Currency Conversion Service
+`http://localhost:8100/currency-conversion/from/USD/to/INR/quantity/10`
---
+++ currency-exchange-service/src/main/java/microservicescourse/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,29 @@
+package microservicescourse.currencyexchangeservice;
+
+import microservicescourse.currencyexchangeservice.CurrencyExchange.CurrencyExchange;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    private Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = new CurrencyExchange(
+                100L, from, to, BigDecimal.valueOf(50)
+        );
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+}
