fb0513c47214d27ce64c02d611238c9be76797b7
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-10-02 23:16:46 -0300

    add currency conversion service

---
+++ currrency-converrsion-service/src/main/java/microservicescourse/currrencyconverrsionmicroservice/CurrrencyConverrsionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package microservicescourse.currrencyconverrsionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrrencyConverrsionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrrencyConverrsionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ currrency-converrsion-service/src/test/java/microservicescourse/currrencyconverrsionmicroservice/CurrrencyConverrsionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicescourse.currrencyconverrsionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrrencyConverrsionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
