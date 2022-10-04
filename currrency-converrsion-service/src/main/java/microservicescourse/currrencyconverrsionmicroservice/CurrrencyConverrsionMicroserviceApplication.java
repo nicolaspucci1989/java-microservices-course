fe74f7d8b32c06f251fc316dfcd7801829d85dfc
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-10-03 22:30:48 -0300

    add feign

--- currrency-converrsion-service/src/main/java/microservicescourse/currrencyconverrsionmicroservice/CurrrencyConverrsionMicroserviceApplication.java
+++ currrency-converrsion-service/src/main/java/microservicescourse/currrencyconverrsionmicroservice/CurrrencyConverrsionMicroserviceApplication.java
@@ -2,8 +2,10 @@ package microservicescourse.currrencyconverrsionmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrrencyConverrsionMicroserviceApplication {
 
 	public static void main(String[] args) {
